eb1d488d1de9a6b125694cff37dfa0c6d97d5302
Olga <114644755+Al-Olga@users.noreply.github.com>
2023-06-20 08:15:07 +0300

    ex_02 added

---
+++ ex_02.java
@@ -0,0 +1,26 @@
+/* Если необходимо, исправьте данный код 
+(задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit) 
+try {
+   int d = 0;
+   double catchedRes1 = intArray[8] / d;
+   System.out.println("catchedRes1 = " + catchedRes1);
+} catch (ArithmeticException e) {
+   System.out.println("Catching exception: " + e);
+} 
+
+Исправления: добавила объявление массива */
+
+import java.util.*;
+
+public class ex_02{
+    public static void main(String[] args) {
+        try {
+            int[] intArray = { 17, 23, 1, 71, 22, 33 };
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+    }
+}
